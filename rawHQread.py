77a0d9bf1774a10f4ea4401b035770f386eed871
PolarizationDude <31082606+PolarizationDude@users.noreply.github.com>
2020-07-25 12:53:22 -0400

    Update rawHQread.py
    Added helper functions to help with exception handling.

--- rawHQread.py
+++ rawHQread.py
@@ -1,9 +1,16 @@
 # -*- coding: utf-8 -*-
 """
 Spyder Editor
+Author: Noah Gilbert
 
-This is a temporary script file.
+This module will open a single existing *.jpeg file and if it was taken with a 
+raspveery pi HQ camera convert the trailing binary data into the bayer grid 
+of 12 bit entries. 
+
 """
+__version__ = 1.0
+__author__ = 'Noah Gilbert'
+
 # Run imports
 import os
 try: 
@@ -14,6 +21,7 @@ except:
     """
     raise ImportError(error_msg)
     
+
 # Define support functions
 def HQjpeg_to_raw(path_in):
     """Convert raw JPEG to numpy.array(dtype=uint16)
@@ -25,7 +33,7 @@ def HQjpeg_to_raw(path_in):
     """    
     file_data = open_pic(path_in)
     # Search the file for the start of the raw data
-    raw_start = file_data.find(b'BRCM')
+    raw_start = find_start(file_data)
     # Separate the file
     file_data = file_data[raw_start:]
     # pull the header info
@@ -58,3 +66,12 @@ def open_pic(path):
         # Read in the image as a binary file to read
         file_data = raw_file.read()
     return file_data
+
+def find_start(file_data):
+    """Search the image for the start of raw data 'BCRM' marker"""
+    # Search the file for the start of the raw data
+    raw_start = file_data.find(b'BRCM')
+    #check if flag was found
+    if raw_start < 0:
+        raise LookupError('Flag "BRCM" not found in jpeg data. File contains no raw data')
+    return raw_start
